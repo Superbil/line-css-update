57eeb09313b4d372f564198032667ed8707ed8dd
Superbil <superbil@gmail.com>
2013-05-28 15:13:28 +0800

    New files
      New        README.org
      New        common.css
      New        likeSelect.css
      New        makeGroup.css
      New        put-in-Line.py
      New        talkAddBuddy.css

---
+++ put-in-Line.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import os.path as path
+from shutil import copyfile
+from glob import glob
+
+
+def update_line_css(source, line_path):
+    "Update Line's CSS from source"
+    line_css_path = "%s/Contents/Resources/skin/mac/css/" % (line_path)
+
+    if path.exists(line_css_path) is False:
+        return
+
+    for filename in glob("%s*.css" % (source)):
+        if __debug__:
+            print "from: %s to %s" % (filename, line_css_path)
+
+        copyfile(filename, line_css_path + filename)
+
+def main():
+    source = "./"
+    line = "/Applications/Line.app-"
+    update_line_css(source, line)
+
+if __name__ == "__main__":
+    main()
